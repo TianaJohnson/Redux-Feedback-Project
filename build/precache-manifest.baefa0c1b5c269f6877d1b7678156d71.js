68d8e8ebfe509bc8f10a318049a57a83c745ac3f
Tiana Johnson <tiana.marie013@gmail.com>
2019-01-28 16:15:40 -0600

    message

---
+++ build/precache-manifest.baefa0c1b5c269f6877d1b7678156d71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89689c607103a430d4d7",
+    "url": "/static/css/main.0912ea04.chunk.css"
+  },
+  {
+    "revision": "89689c607103a430d4d7",
+    "url": "/static/js/main.89689c60.chunk.js"
+  },
+  {
+    "revision": "ef43b4e4aa7250b3a203",
+    "url": "/static/js/1.ef43b4e4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7632109a19e7a643d7dd0165bda61ce",
+    "url": "/index.html"
+  }
+];
